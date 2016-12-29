17f858dcbace46665a1f67daa26426456a1a5b6a
User <liptugamichael@gmail.com>
2016-12-29 17:18:18 +0200

    fixed bug

--- JSQMessagesViewController/Views/JSQMessagesCollectionViewCellOutgoing.m
+++ JSQMessagesViewController/Views/JSQMessagesCollectionViewCellOutgoing.m
@@ -24,6 +24,7 @@
 
 - (void)awakeFromNib
 {
+    
     [super awakeFromNib];
     self.messageBubbleTopLabel.textAlignment = NSTextAlignmentRight;
     self.cellBottomLabel.textAlignment = NSTextAlignmentRight;
